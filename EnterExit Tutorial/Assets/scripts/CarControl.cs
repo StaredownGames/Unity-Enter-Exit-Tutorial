112e9478677b4a613643cc6016d0c72a0e92b419
StaredownGames <44645729+StaredownGames@users.noreply.github.com>
2018-10-31 12:12:06 -0400

    Commit 1
    Default starter files.

---
+++ EnterExit Tutorial/Assets/scripts/CarControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour 
+{
+	public float speed = 15f;
+	public int turnSpeed = 20;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float axisH = Input.GetAxis("Horizontal");
+		float axisV = Input.GetAxis("Vertical");
+		transform.Translate(Vector3.forward * axisV * speed * Time.deltaTime);
+		if (axisV > 0) 
+		{
+			transform.Rotate(Vector3.up * axisH * turnSpeed * Time.deltaTime);
+		}
+		else
+		{
+			transform.Rotate(-Vector3.up * axisH * turnSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ EnterExit Tutorial/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+	public float speed = 5;
+	public float turnSpeed = 40;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float axisH = Input.GetAxis("Horizontal");	
+		float axisV = Input.GetAxis("Vertical");
+
+		transform.Translate(Vector3.forward * axisV * speed * Time.deltaTime);
+		transform.Rotate(Vector3.up * axisH * turnSpeed * Time.deltaTime);
+	}
+}
